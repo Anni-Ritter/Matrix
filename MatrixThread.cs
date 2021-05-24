daa17dff8585bd1f0a1e519c99a503c4db7a2fe3
AnNi_RiTtEr <57864367+Anni-Ritter@users.noreply.github.com>
2021-05-24 14:49:51 +0300

    Add files via upload
    Fixed

---
+++ MatrixThread.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace Matrix
+{
+    public class MatrixThread
+    {
+        private int[,] FirstMatrix;
+        private int[,] SecondMatrix;
+        private int Size;
+
+
+        public MatrixThread(int[,] firstMatrix, int[,] secondMatrix, int size)
+        {
+            this.FirstMatrix = firstMatrix;
+            this.SecondMatrix = secondMatrix;
+            this.Size = size;
+        }
+
+        public void CalculateValue(int row, int col)
+        {
+            Program.ResultMatrix[row, col] = 0;
+            for (int i = 0; i < Size; ++i)
+            {
+                Program.ResultMatrix[row, col] += FirstMatrix[row, i] * SecondMatrix[i, col];
+            }
+        }
+        
+        public void MultipleMatrixThread(int firstIndex, int lastIndex)
+        {
+        
+            for (int index = firstIndex; index < lastIndex; ++index)
+            {
+                CalculateValue(index / Size, index % Size);
+            }
+            
+        }
+    }    
+}
